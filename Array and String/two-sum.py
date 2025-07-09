1dd54634059cf9f2835528e54629335e1f4d1b29
Andrew Tu <andrewtu335@gmail.com>
2025-07-09 15:25:47 +0800

    solution to two-sum problems

---
+++ Array and String/two-sum.py
@@ -0,0 +1,43 @@
+"""
+given the array and target, find the index of two elements in
+array that add up to the target
+
+ex:
+input:
+array = [1,2,3,5,3,6,7,9,-1,-3]
+target = 6
+
+output:[0,3]
+"""
+
+def two_sum(array:list[int], target:int):
+    map = dict()
+    for i, val in enumerate(array):
+        if val in map:
+            return [map[val], i]
+        else:
+            complement = target - val
+            map[complement] = i
+    return []
+
+"""
+find all unique index pair for two sum problem
+(discard the duplicate index pair)
+"""
+
+def two_sum_with_all_pairs(array:list[int], target:int):
+    map = dict()
+    results = []
+    for i, val in enumerate(array):
+        if val in map:
+            results.append((map[val], i))
+        else:
+            complement = target - val
+            map[complement] = i
+    return results
+
+if __name__ == "__main__":
+    array = [1,2,3,5,3,6,7,9,-1,-3]
+    target = 6
+    print("two sum output: ", two_sum(array, target))
+    print("two sum with all index pairs output: ", two_sum_with_all_pairs(array, target))
